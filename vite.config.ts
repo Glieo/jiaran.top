812a17649f8237852705723f9be284268510aea7
lieo <liyhx@qq.com>
2022-03-01 00:30:50 +0800

    更新简介

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    assetsInlineLimit: 65536,
+  },
+
   plugins: [
     vue(),
+
     VitePWA({
       includeAssets: [
         "404.html",
@@ -45,8 +52,4 @@ export default defineConfig({
       },
     }),
   ],
-  base: "./",
-  build: {
-    assetsInlineLimit: 65536,
-  },
 });
